25f5aa1b171f8499cb488ccba5d885ad0d2dff3e
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 <stankevichuuladimiru@gmail.com>
2021-08-28 02:35:05 +0300

    fix: Things will look better in the morning...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+      str = str.split('');
+      let dump = str.concat(bracketsConfig.flat(10));
+      let source = [];
+      const pass = ['(', '[', '{',')', ']', '}',];
+      console.log(str);
+      console.log(bracketsConfig);
+      console.log(dump);
+      
+      for (let j = 0; j < dump.length; j = j + 1){
+         let charDump = dump[j];
+         if (pass.includes(charDump)){
+          source.push(charDump);
+                 }else{
+                   continue;
+                 }
+  if (source.length === 0) return false;
+      };
+
+    
+      
+      console.log(source);
+      
+      const openBrackets = ['(', '[', '{', '|',];
+ 
+      const pairBrackets = {
+        [')']: '(',
+        [']']: '[',
+        ['}']: '{',
+        ['|']: '|',
+      };
+         let stack = [];
+          for (let i = 0; i < source.length; i = i + 1){
+          let charSource = source[i];
+         
+       if (openBrackets.includes(charSource)){
+            stack.push(charSource);
+       }else{
+         if(stack.length === 0){
+           return false;
+         }
+        
+         
+         let topChar = stack[stack.length-1];
+         
+         if (pairBrackets[charSource] === topChar){
+           stack.pop();
+           console.log(topChar);
+         }else{
+           return false;
+         }
+       }
+  }
+  return stack.length ===0;
 }
+
